21e8fb868708eb2f1967b2abf99431a068e09a56
Isaac Koh <kohvern@outlook.com>
2025-06-05 13:52:28 +0800

    debug for amazon xray tracing

--- payment/obj/Debug/net9.0/payment.AssemblyInfo.cs
+++ payment/obj/Debug/net9.0/payment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("payment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faeb5ea7113f54b6e237e95bb34d46947cd4e5fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b241b17987a48eb0369f5db9c5f159c614e06e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("payment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("payment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
