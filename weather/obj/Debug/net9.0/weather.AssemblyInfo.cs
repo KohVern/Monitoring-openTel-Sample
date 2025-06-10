76878a5fd40fd0712a76f8806fda43fde9fbb97b
Isaac Koh <kohvern@outlook.com>
2025-06-10 15:11:44 +0800

    Jenkins demo

--- weather/obj/Debug/net9.0/weather.AssemblyInfo.cs
+++ weather/obj/Debug/net9.0/weather.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("weather")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d238e224859924a506fe68f6a07cd83bb39d0caa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+203b7644250915a3614f6693055a68faba039bf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("weather")]
 [assembly: System.Reflection.AssemblyTitleAttribute("weather")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
