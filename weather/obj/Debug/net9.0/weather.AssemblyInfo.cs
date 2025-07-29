3ba2f3a851a1bd88a80fbb4f42e32e58be0f557b
Isaac Koh <kohvern@outlook.com>
2025-07-29 10:54:43 +0800

    Added Loki + Fluentbit working

--- weather/obj/Debug/net9.0/weather.AssemblyInfo.cs
+++ weather/obj/Debug/net9.0/weather.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("weather")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce4162ac0a4a63373781e1d8ff95b185daed8274")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05f9c49e32cc7e982bd24e68bd123a7db00d2769")]
 [assembly: System.Reflection.AssemblyProductAttribute("weather")]
 [assembly: System.Reflection.AssemblyTitleAttribute("weather")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
